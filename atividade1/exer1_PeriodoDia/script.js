c1be3f4ea493051d46642372753ee0de857b6235
Taislaine Massucato <massucato2@outlook.com>
2024-04-06 08:41:05 -0300

    Correção do bug com imagens

--- atividade1/exer1_PeriodoDia/script.js
+++ atividade1/exer1_PeriodoDia/script.js
@@ -1,22 +1,22 @@
 function carregar(){
     var msgm = document.getElementById('msg');
-    var fots = document.getElementById('foto');
+    var fots = document.getElementById('imagem');
     var data = new Date();
-   var hora =  6 // data.getHours();
+   var hora =  20 // data.getHours();
     msgm.innerHTML = `olá agora são ${hora}`
    
 if(hora >= 0 && hora <= 4){
     alert('Boa madrugada');
-    fots.src = "../exer1_PeriodoDia/manha.jpg"
+    fots.src = "manha.jpg"
 }else if(hora > 4 && hora < 12){
     alert('Bom dia !!')
-    fots.src = "../exer1_PeriodoDia/manha.jpg"
+    fots.src = "manha.jpg"
 }else if(hora >=12 && hora <= 18){
     alert('Boa tarde !!')
-    fots.src = "../exer1_PeriodoDia/tarde.jpg"
+    fots.src = "tarde.jpg"
 }else if(hora >=19 && hora < 24){
     alert('Boa noitee!!')
-    fots.src = "../exer1_PeriodoDia/noite.jpg"
+    fots.src = "noite.jpg"
 }
 
 }
